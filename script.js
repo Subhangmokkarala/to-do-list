6d30fcf2413193d06b0fe5cd3d8aeddd7589ef7a
Subhang Mokkarala <90596655+Subhangmokkarala@users.noreply.github.com>
2023-06-29 15:42:11 +0530

    push rich ui

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+  const modeToggle = document.querySelector('.mode-toggle');
+  modeToggle.textContent = body.classList.contains('dark-mode') ? 'Toggle Light Mode' : 'Toggle Dark Mode';
+}
+
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getTasksFromLocalStorage() {
+  var storedTasks = localStorage.getItem('tasks');
+  return storedTasks ? JSON.parse(storedTasks) : [];
+}
+
 function addTask() {
   var input = document.getElementById('taskInput');
   var task = input.value;
@@ -17,9 +33,12 @@ function addTask() {
     li.classList.toggle('deleted');
     if (li.classList.contains('deleted')) {
       deleteBtn.innerHTML = '♻️';
+      tickBtn.classList.add('deleted');
     } else {
       deleteBtn.innerHTML = 'X';
+      tickBtn.classList.remove('deleted');
     }
+    updateTasksInLocalStorage();
   });
 
   var tickBtn = document.createElement('button');
@@ -33,6 +52,7 @@ function addTask() {
       // Toggle completion
       li.classList.toggle('completed');
     }
+    updateTasksInLocalStorage();
   });
 
   var restoreBtn = document.createElement('button');
@@ -41,6 +61,8 @@ function addTask() {
   restoreBtn.addEventListener('click', function() {
     li.classList.remove('deleted');
     deleteBtn.innerHTML = 'X';
+    tickBtn.classList.remove('deleted');
+    updateTasksInLocalStorage();
   });
 
   var buttonsContainer = document.createElement('div');
@@ -50,10 +72,18 @@ function addTask() {
 
   if (li.classList.contains('deleted')) {
     buttonsContainer.appendChild(restoreBtn);
+    tickBtn.classList.add('deleted');
   }
 
   li.appendChild(buttonsContainer);
   taskList.appendChild(li);
+
+  // Retrieve tasks from local storage
+  var tasks = getTasksFromLocalStorage();
+  tasks.push(task);
+
+  // Save updated tasks to local storage
+  saveTasksToLocalStorage(tasks);
 
   input.value = '';
 
@@ -65,88 +95,137 @@ function addTask() {
   }
 }
 
-function exportTasks() {
+function updateTasksInLocalStorage() {
+  var tasks = [];
   var taskList = document.getElementById('taskList');
-  var tasks = taskList.getElementsByTagName('li');
+  var tasksElements = taskList.getElementsByTagName('li');
 
-  if (tasks.length === 0) {
-    alert('No tasks to export.');
-    return;
+  for (var i = 0; i < tasksElements.length; i++) {
+    var taskText = tasksElements[i].textContent;
+    tasks.push(taskText);
   }
 
-  var csvContent = 'data:text/csv;charset=utf-8,';
-  var rows = [];
+  saveTasksToLocalStorage(tasks);
+}
 
-  for (var i = 0; i < tasks.length; i++) {
-    var taskText = tasks[i].textContent.replace(/,/g, ''); // Remove commas from task text
-    rows.push('"' + taskText + '"');
+function displayTasksFromLocalStorage() {
+  var tasks = getTasksFromLocalStorage();
+  var taskList = document.getElementById('taskList');
+
+  tasks.forEach(function(task) {
+    var li = document.createElement('li');
+    li.textContent = task;
+
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'delete-btn';
+    deleteBtn.innerHTML = 'X';
+    deleteBtn.addEventListener('click', function() {
+      li.classList.toggle('deleted');
+      if (li.classList.contains('deleted')) {
+        deleteBtn.innerHTML = '♻️';
+        tickBtn.classList.add('deleted');
+      } else {
+        deleteBtn.innerHTML = 'X';
+        tickBtn.classList.remove('deleted');
+      }
+      updateTasksInLocalStorage();
+    });
+
+    var tickBtn = document.createElement('button');
+    tickBtn.className = 'tick-btn';
+    tickBtn.innerHTML = '&#10004;';
+    tickBtn.addEventListener('click', function() {
+      if (li.classList.contains('deleted')) {
+        // Permanent delete
+        li.remove();
+      } else {
+        // Toggle completion
+        li.classList.toggle('completed');
+      }
+      updateTasksInLocalStorage();
+    });
+
+    var restoreBtn = document.createElement('button');
+    restoreBtn.className = 'restore-btn';
+    restoreBtn.innerHTML = '♻️';
+    restoreBtn.addEventListener('click', function() {
+      li.classList.remove('deleted');
+      deleteBtn.innerHTML = 'X';
+      tickBtn.classList.remove('deleted');
+      updateTasksInLocalStorage();
+    });
+
+    var buttonsContainer = document.createElement('div');
+    buttonsContainer.className = 'buttons-container';
+    buttonsContainer.appendChild(tickBtn);
+    buttonsContainer.appendChild(deleteBtn);
+
+    if (li.classList.contains('deleted')) {
+      buttonsContainer.appendChild(restoreBtn);
+      tickBtn.classList.add('deleted');
+    }
+
+    li.appendChild(buttonsContainer);
+    taskList.appendChild(li);
+  });
+
+  // Check if there are no tasks
+  if (taskList.children.length === 0) {
+    displayNoTasksMessage(true);
+  } else {
+    displayNoTasksMessage(false);
   }
-
-  csvContent += rows.join('\n');
-
-  var encodedUri = encodeURI(csvContent);
-  var link = document.createElement('a');
-  link.setAttribute('href', encodedUri);
-  link.setAttribute('download', 'tasks.csv');
-  link.style.display = 'none';
-
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
 }
 
 function displayNoTasksMessage(show) {
-  var messageContainer = document.getElementById('noTasksMessage');
-
+  var noTasksMessage = document.getElementById('noTasksMessage');
   if (show) {
-    // Create message and image elements
-    var messageText = document.createElement('p');
-    messageText.textContent = "Seems like you are free today";
-    var image = document.createElement('img');
-    image.src = 'path/to/your/image.png';
-    image.alt = 'No tasks image';
-
-    // Append elements to the message container
-    messageContainer.appendChild(messageText);
-    messageContainer.appendChild(image);
-
-    // Show the message container
-    messageContainer.style.display = 'block';
+    noTasksMessage.classList.remove('hidden');
   } else {
-    // Hide the message container
-    messageContainer.style.display = 'none';
+    noTasksMessage.classList.add('hidden');
+  }
+}
 
-    // Clear any existing message and image elements
-    messageContainer.innerHTML = '';
+function searchTasks() {
+  var searchInput = document.getElementById('searchInput');
+  var filter = searchInput.value.toUpperCase();
+  var taskList = document.getElementById('taskList');
+  var tasks = taskList.getElementsByTagName('li');
+
+  for (var i = 0; i < tasks.length; i++) {
+    var task = tasks[i];
+    var taskText = task.textContent || task.innerText;
+    if (taskText.toUpperCase().indexOf(filter) > -1) {
+      task.style.display = '';
+    } else {
+      task.style.display = 'none';
+    }
   }
 }
 
 function exportTasks() {
+  var tasks = [];
   var taskList = document.getElementById('taskList');
-  var tasks = taskList.getElementsByTagName('li');
+  var tasksElements = taskList.getElementsByTagName('li');
 
-  if (tasks.length === 0) {
-    alert('No tasks to export.');
-    return;
+  for (var i = 0; i < tasksElements.length; i++) {
+    var taskText = tasksElements[i].textContent;
+    tasks.push(taskText);
   }
 
-  var csvContent = 'data:text/csv;charset=utf-8,';
-  var rows = [];
+  var tasksString = tasks.join('\n');
 
-  for (var i = 0; i < tasks.length; i++) {
-    var taskText = tasks[i].textContent.replace(/,/g, ''); // Remove commas from task text
-    rows.push('"' + taskText + '"');
-  }
-
-  csvContent += rows.join('\n');
-
-  var encodedUri = encodeURI(csvContent);
-  var link = document.createElement('a');
-  link.setAttribute('href', encodedUri);
-  link.setAttribute('download', 'tasks.csv');
-  link.style.display = 'none';
-
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+  var downloadLink = document.createElement('a');
+  downloadLink.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(tasksString);
+  downloadLink.download = 'tasks.txt';
+  downloadLink.style.display = 'none';
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
 }
+
+// Check if there are no tasks initially
+displayNoTasksMessage(true);
+
+// Display tasks from local storage
+displayTasksFromLocalStorage();
